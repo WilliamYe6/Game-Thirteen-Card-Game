6cddc0a6e928bf6c77952d0afd89260deb152eaf
christine-yd <christine.yang-dai@mail.mcgill.ca>
2020-11-09 13:51:57 -0500

    Update ai_player.py

---
+++ ai_player.py
@@ -0,0 +1,7 @@
+
+
+def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
+    # your code here
+
+def discard(hand, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
+    # your code here
