a20474b7a2a14cd5458e9ad73e60ea787f81df31
aashihaF20 <74200627+aashihaF20@users.noreply.github.com>
2020-11-09 14:02:56 -0500

    Update ai_player.py
    added fct description (testing)

--- ai_player.py
+++ ai_player.py
@@ -1,6 +1,12 @@
 
 
 def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
+    '''
+    This function decides from where to pick up a card: from the discard pile or
+    from the stock. It must return either the string stock or discard. Note that it can only draw from the
+    discard pile if the top_discard card is not None. To aid in your decision, the function has the following
+    parameters
+    '''
     # your code here
 
 def discard(hand, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
