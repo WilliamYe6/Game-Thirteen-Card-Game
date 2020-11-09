74f092e27d3adf5ef3a1427efbaed93ac0400629
christine-yd <christine.yang-dai@mail.mcgill.ca>
2020-11-09 14:15:45 -0500

    Update ai_player.py
    edited draw docstring

--- ai_player.py
+++ ai_player.py
@@ -1,7 +1,7 @@
 
 
 def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
-    '''(list, int, str, list, int, int, int) -> str
+    '''(list, int, bool, list, int, int, int) -> str
 
     This function decides from where to pick up a card: from the discard pile or
     from the stock. It must return either the string stock or discard. Note that it can only draw from the
