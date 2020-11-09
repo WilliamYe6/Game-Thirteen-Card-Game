2560f51a66168c706eb7be3a2a88047f8a89b2e2
christine-yd <christine.yang-dai@mail.mcgill.ca>
2020-11-09 14:06:27 -0500

    Update ai_player.py
    testing a new branch, to undertand how to merge, edited the docstring of draw

--- ai_player.py
+++ ai_player.py
@@ -1,7 +1,8 @@
 
 
 def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
-    '''
+    '''(list, int, str, list, int, int, int) -> str
+
     This function decides from where to pick up a card: from the discard pile or
     from the stock. It must return either the string stock or discard. Note that it can only draw from the
     discard pile if the top_discard card is not None. To aid in your decision, the function has the following
@@ -11,3 +12,4 @@ def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position,
 
 def discard(hand, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
     # your code here
+    
