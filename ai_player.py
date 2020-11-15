0fa844d9a54401d53fd447cd084a81e187834006
christine-yd <christine.yang-dai@mail.mcgill.ca>
2020-11-15 13:09:39 -0500

    Update ai_player.py

--- ai_player.py
+++ ai_player.py
@@ -4,42 +4,42 @@ from card import *
 from arrangement import *
 import doctest
 
-def potential_arrangement(a_hand, wildcard_rank):
+def complete_arrangement(a_hand, wildcard_rank):
     '''(list, int) -> list
 
     Given a hand, returns a list of the cards that could potentially complete an arrangement.
     
-    >>> potential_arrangement([1, 5, 9, 13], 10) #wildcard not involved, 1 seq 
+    >>> complete_arrangement([1, 5, 9, 13], 10) #wildcard not involved, 1 seq 
     [1, 5, 9, 13, 17]
 
-    >>> potential_arrangement([1, 5, 9, 13], 3) #wildcard involved, 1 seq
+    >>> complete_arrangement([1, 5, 9, 13], 3) #wildcard involved, 1 seq
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-    >>> potential_arrangement([25, 26, 27], 10) #wildcard not involved, 1 group
+    >>> complete_arrangement([25, 26, 27], 10) #wildcard not involved, 1 group
     [25, 26, 27, 28]
 
-    >>> potential_arrangement([25, 26, 27], 6) #wildcard involved, 1 group
+    >>> complete_arrangement([25, 26, 27], 6) #wildcard involved, 1 group
     [21, 22, 23, 25, 26, 27, 28, 29, 30, 31]
 
-    >>> potential_arrangement([1, 2, 9, 13], 10) #wildcard not involved, 1 potential group (1 & 2 same rank)
+    >>> complete_arrangement([1, 2, 9, 13], 10) #wildcard not involved, 1 potential group (1 & 2 same rank)
     [1, 2, 3, 4, 5, 17]
 
-    >>> potential_arrangement([32, 32, 48, 13], 11) #wildcard involved, 1 potential group (32 & 32 same rank)
+    >>> complete_arrangement([32, 32, 48, 13], 11) #wildcard involved, 1 potential group (32 & 32 same rank)
     [9, 13, 14, 15, 16, 17, 28, 29, 30, 31, 32, 36, 44, 45, 46, 47, 48, 52]
 
-    >>> potential_arrangement([2, 3, 4, 8, 12], 10) #wildcard not involved, 1 group (2, 3, 4) and 1 seq (4, 8, 12)
+    >>> complete_arrangement([2, 3, 4, 8, 12], 10) #wildcard not involved, 1 group (2, 3, 4) and 1 seq (4, 8, 12)
     [1, 2, 3, 4, 8, 12, 16]
 
-    >>> potential_arrangement([2, 3, 4, 8, 12], 1) #wildcard involved, 1 group (2, 3, 4) and 1 seq (4, 8, 12)
+    >>> complete_arrangement([2, 3, 4, 8, 12], 1) #wildcard involved, 1 group (2, 3, 4) and 1 seq (4, 8, 12)
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16]
 
-    >>> potential_arrangement([16, 24, 33, 47], 0) #wildcard not involved, 1 potential seq (16 & 24)
+    >>> complete_arrangement([16, 24, 33, 47], 0) #wildcard not involved, 1 potential seq (16 & 24)
     [20]
 
-    >>> potential_arrangement([16, 24, 33, 47], 11) #wildcard involved, 1 potential seq (16 & 24)
+    >>> complete_arrangement([16, 24, 33, 47], 11) #wildcard involved, 1 potential seq (16 & 24)
     [12, 13, 14, 15, 16, 20, 21, 22, 23, 24, 28, 29, 33, 34, 35, 36, 37, 45, 46, 47, 48, 51]
 
-    >>> potential_arrangement([16, 24, 33, 12], 8) #wildcard involved, multiple potential seq (16, 24, 12)
+    >>> complete_arrangement([16, 24, 33, 12], 8) #wildcard involved, multiple potential seq (16, 24, 12)
     [8, 9, 10, 11, 12, 13, 14, 15, 16, 20, 21, 22, 23, 24, 28, 29, 33, 34, 35, 36, 37]
     
     '''
@@ -56,7 +56,7 @@ def potential_arrangement(a_hand, wildcard_rank):
             for i in range(len(a_hand)):
                 
                 #any same_suit with rank +- 1 gives an sequence
-                if get_rank(a_hand[i]) < 13:
+                if get_rank(a_hand[i]) < 12:
                     wanted_cards += [get_card(get_suit(a_hand[i]), (get_rank(a_hand[i])) + 1)]
                 if get_rank(a_hand[i]) > 0:
                     wanted_cards += [get_card(get_suit(a_hand[i]), (get_rank(a_hand[i])) - 1)]
@@ -82,7 +82,7 @@ def potential_arrangement(a_hand, wildcard_rank):
                 max_card = max([get_rank(a_hand[j]),get_rank(a_hand[i])])
                 min_card = min([get_rank(a_hand[j]),get_rank(a_hand[i])])
                 
-                if max_card < 13:
+                if max_card < 12:
                     wanted_cards += [get_card(get_suit(a_hand[i]), (max_card) + 1)]
                     
                 if min_card > 0:
@@ -127,7 +127,6 @@ def single_card_points(card):
         return 10
     elif get_rank(card) in RANKS[12:]:
         return 1
-
 
 def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position, wildcard_rank, num_turns_this_round):
     '''(list, int, bool, list, int, int, int) -> str
@@ -174,32 +173,25 @@ def draw(hand, top_discard, last_turn, picked_up_discard_cards, player_position,
         return 'discard'
     
     #CONDITION 3: Not last turn and top card of discard pile doesn't form any arrangement
-    if not last_turn and top_discard not in potential_arrangement(hand, wildcard_rank):
+    if not last_turn and top_discard not in complete_arrangement(hand, wildcard_rank):
         return 'stock'
     
     #WILL'S added part
     # CONDITION 4: If it is last round and wildcard cannot form a group/seq with card(s) from hand
-    if (last_round) and (potential_arragement(hand, wildcard_rank) == hand) and ((top_discard not in potential_arrangement(hand, wildcard_rank))):
+    if last_turn and complete_arrangement(hand, wildcard_rank) == hand and (top_discard not in complete_arrangement(hand, wildcard_rank)):
         return 'stock'
     
-    
-    #PUT THIS IN THE END SINCE IT HAS AN ELSE STATEMENT!!!
-    #****** Changed for a for loop instead (if conditions are not met, it will return stock so acts
-    #like an else statement, hope that's okay! - Julia)
+    #Top card on discard pile can form an arrangement
+    if top_discard in complete_arrangement(hand, wildcard_rank):
+        return 'discard'
     
     #Iterating through each card of the hand
     for card in range(len(hand)):
         
-        #Top card on discard pile can form an arrangement
-        if top_discard in potential_arrangement(hand, wildcard_rank):
-            return 'discard'
-        
-        
-        
         #If it is the last turn and one card from the hand can't form an arrangement and is worth
         #more points than the top card on the discard pile (pick up card from discard pile and
         #discard the card that is worth more points)
-        elif (last_turn and hand[card] not in potential_arrangement(hand, wildcard_rank) and
+        if (last_turn and hand[card] not in complete_arrangement(hand, wildcard_rank) and
               single_card_points(top_discard)<single_card_points(hand[card])):
             return 'discard'
         
@@ -227,7 +219,7 @@ def discard(hand, last_turn, picked_up_discard_cards, player_position, wildcard_
     discard_cards = []
     for card in hand:
         #cards in our hand that are not part of a sequence nor a group
-        if card not in get_arrangement(hand, wildcard_rank):
+        if card not in get_arrangement(tuple(hand), wildcard_rank):  #and not potential_arrangement
             discard_cards += [card]
     
     #list containing "penalty" points associated to the cards that can be discarded
@@ -242,15 +234,13 @@ def discard(hand, last_turn, picked_up_discard_cards, player_position, wildcard_
         #the penalty point associated to each card in ours hand that is not a part of an arrangement
         points = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 1]
         discard_value += [points[RANKS.index(get_rank(discard_cards[i]))]*turn_multiplier]
-        '''
-        if player_position != : #to do, if the player is the last player to play before the round ends,
-        then don't care about the following:   need to write the code for this - Aashiha&Christine
-        '''
-        #discarding cards in potential_arrangement gives an advantage to other players,
+        
+        #discarding cards in complete_arrangement gives an advantage to other players,
         for others_hand in picked_up_discard_cards:
-            if discard_cards[i] in potential_arrangement(others_hand, wildcard_rank):
-                discard_value[i] = discard_value[i] / 2 #TBF********** #so the penalty value is disminished
+                for cards in others_hand:
+                    others_hand.count(cards) > 1
+            #if discard_cards[i] in complete_arrangement(others_hand, wildcard_rank):
+                    discard_value[i] = discard_value[i] / 2 #TBF********** #so the penalty value is disminished'''
                 
     #the card with the highest value will be discarded
     return discard_cards[discard_value.index(max(discard_value))]
-
